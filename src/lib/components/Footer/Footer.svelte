1d5a52d0523f03e628662f45057e6ebb47957989
medecinelibre <contact@medecinelibre.com>
2023-11-20 09:28:45 +0300

    add phone to footer, add info to CPTS page

--- src/lib/components/Footer/Footer.svelte
+++ src/lib/components/Footer/Footer.svelte
@@ -12,7 +12,7 @@
 		faGithub,
 		faMastodon
 	} from '@fortawesome/free-brands-svg-icons';
-
+	import { faPhone } from '@fortawesome/free-solid-svg-icons';
 	// Base Classes
 	const cBase =
 		'bg-surface-50 dark:bg-surface-900 border-t border-surface-500/10 text-xs md:text-base';
@@ -28,11 +28,16 @@
 	<div class="w-full max-w-7xl mx-auto p-4 py-16 md:py-24 space-y-10">
 		<!-- Row 1 -->
 		<section class={cRowOne}>
-			<div
-				class="grid grid-cols-1 gap-2 place-content-center place-items-center md:place-items-start"
-			>
-				<LogoIcon />
-				<p class="!text-sm opacity-80">Applications & sites web pour la santé pluripro.</p>
+			<div class="flex flex-col space-y-2">
+				<div class="flex space-x-2 items-center">
+					<LogoIcon />
+					<h3 class="h3">Médecine Libre</h3>
+				</div>
+				<div>
+					<p class="!text-sm opacity-80">Applications & sites web pour la santé pluripro.</p>
+				</div>
+				<a href="tel:+33977215435"><div class="flex space-x-2 items-center"><span><Fa icon={faPhone} size="xs"/></span><span>0977215435</span></div></a>
+				
 			</div>
 			<div class="hidden md:grid grid-cols-3 gap-8">
 				<div class="space-y-6">
@@ -87,7 +92,8 @@
 
 		<!-- Row 2 -->
 		<section class={cRowTwo}>
-			<p><!--
+			<p>
+				<!--
 				<a
 					class="anchor"
 					href="https://github.com/skeletonlabs/skeleton/blob/master/LICENSE"
